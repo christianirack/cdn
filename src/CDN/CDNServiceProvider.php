ff41a3c831e7023bc071404040964a62265abf8e
Christian Irack <christian.irack@ktc.com.mx>
2017-12-06 10:48:53 -0600

    Update.

--- src/CDN/CDNServiceProvider.php
+++ src/CDN/CDNServiceProvider.php
@@ -2,10 +2,40 @@
 
 namespace CDN;
 
-class CDNServiceProvider {
-
-    public function index(){
-        return 'Hi!';
+    use Illuminate\Support\ServiceProvider;
+    use League\Flysystem\Filesystem;
+    use League\Flysystem\Sftp\SftpAdapter;
+    
+    class CDNServiceProvider extends ServiceProvider{
+        /**
+         * Perform post-registration booting of services.
+         *
+         * @return void
+         */
+        public function boot()
+        {
+            
+            die(base_path());
+            /*
+            $filesystem = $this->app->make('filesystem');
+    
+            $filesystem->extend('sftp', function($app, $config) {
+                return new Filesystem(new SftpAdapter($config));
+            });
+            */
+    
+        }
+    
+    
+    
+        /**
+         * Register bindings in the container.
+         *
+         * @return void
+         */
+        public function register()
+        {
+            //
+        }
     }
-
-}
+    
