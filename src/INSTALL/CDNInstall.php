9460d2bf57a26a5334b028cbd4ffa21c6cfaefc1
Christian Irack <christian.irack@ktc.com.mx>
2017-12-06 15:19:46 -0600

    Update

---
+++ src/INSTALL/CDNInstall.php
@@ -0,0 +1,53 @@
+<?php
+
+    namespace CDN\INSTALL;
+
+    use Illuminate\Support\Facades\Config;
+
+    use Illuminate\Support\ServiceProvider;
+    use League\Flysystem\Filesystem;
+    //use League\Flysystem\Sftp\SftpAdapter;
+    use CDN\SftpCustomAdapter;
+    
+    class CDNInstall extends ServiceProvider{
+        /**
+         * Perform post-registration booting of services.
+         *
+         * @return void
+         */
+        public function boot()
+        {
+            
+           // die(base_path());
+
+           
+          
+            $filesystem = $this->app->make('filesystem');
+    
+            $filesystem->extend('sftp', function($app, $config) {
+                return new Filesystem(new SftpCustomAdapter($config));
+            });
+
+            
+    
+        }
+
+        public function install(){
+
+            copy(__dir__."/TEMPLATES/hi.php", base_path()."/hi".time().".php");
+
+        }
+    
+    
+    
+        /**
+         * Register bindings in the container.
+         *
+         * @return void
+         */
+        public function register()
+        {
+            //
+        }
+    }
+    
